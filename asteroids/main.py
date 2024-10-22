6332f6ebaab4cc6510da76a3a62137cc7068650c
Leighton <LeightonShervington@gmail.com>
2024-10-22 17:14:54 -0400

    Player class, draw player on surface

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 
@@ -10,7 +11,9 @@ def main():
     Screen height: {SCREEN_HEIGHT}''')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
 
     while pygame.display.get_init():
         #gameloop
@@ -18,7 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+        
 
         #60 fps
         dt = clock.tick(60) / 1000
