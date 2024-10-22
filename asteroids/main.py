e8ee9e11b1066a89371e80a1cfddeb0dde1c9d35
Leighton <LeightonShervington@gmail.com>
2024-10-22 16:21:47 -0400

    create gameloop, limit FPS to 60

--- asteroids/main.py
+++ asteroids/main.py
@@ -9,13 +9,19 @@ def main():
     Screen width: {SCREEN_WIDTH}
     Screen height: {SCREEN_HEIGHT}''')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while pygame.display.get_init():
+        #gameloop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
         pygame.display.flip()
+
+        #60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
