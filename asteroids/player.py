512278b80d819fd1be00512d4301cce169bf2640
Leighton <LeightonShervington@gmail.com>
2024-10-27 13:22:25 -0400

    Added shoot cooldown mechanic

--- asteroids/player.py
+++ asteroids/player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
@@ -29,11 +30,16 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt*-1)
@@ -45,6 +51,7 @@ class Player(CircleShape):
             self.move(dt*-1)
         if keys[pygame.K_SPACE]:
             self.shoot()
+
 
 class Shots(CircleShape):
     def __initi__(self, x, y):
